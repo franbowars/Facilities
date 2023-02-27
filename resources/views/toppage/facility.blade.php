9d9ae63ed7a586103ab940c58697daaa54d9c685
y-adachi <yfe27665@gmail.com>
2023-02-27 14:00:51 +0000

    Day6

--- resources/views/toppage/facility.blade.php
+++ resources/views/toppage/facility.blade.php
@@ -1,31 +1,54 @@
-<section>
-    <h2 class="c-heading--page" id="table">東武動物公園の詳細情報</h2>
-        <div class="c-container--sm">
-            <table class="c-table c-table--border c-table--sm">
-                <tbody>
-                    
-                    <tr class="c-table__tr">
-                        <th class="c-table__th c-table__th--gray c-table--left">名称</th>
-                            <td class="c-table__td"><p class="c-word">{{ $facility->name }}</p></td>
-                    </tr>
-                    <tr class="c-table__tr">
-                        <th class="c-table__th c-table__th--gray c-table--left">住所</th>
-                            <td class="c-table__td">{{ $facility->address }}</td>
-                    </tr>
-                    <tr class="c-table__tr">
-                        <th class="c-table__th c-table__th--gray c-table--left">施設内容</th>
-                            <td class="c-table__td">{{ $facility->content }}</td>
-                    </tr>
-                     <tr class="c-table__tr">
-                         <th class="c-table__th c-table__th--gray c-table--left">施設設備</th>
-                            <td class="c-table__td">{{ $facility->equipment }}</td>
-                    </tr>
-                    <tr class="c-table__tr">
-                        <th class="c-table__th c-table__th--gray c-table--left">施設料金</th>
-                            <td class="c-table__td">{{ $facility->fee }}</td>
-                    </tr>
-                    <tr class="c-table__tr">
-                        <th class="c-table__th c-table__th--gray c-table--left">施設アクセス</th>
-                        <th class="c-table__th c-table__th--gray c-table--left">{{ $facility->access }}</th>
-                    </tr>
-</section>
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Scripts -->
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/facility.css') }}" rel="stylesheet">
+    </head>   
+    <body>
+        <section>
+            <h2>施設の詳細情報</h2>
+                <div class="container">
+                    <table class="table table-hover">
+                        <tbody>
+                            <tr class="table-warning">
+                                <th>名称</th>
+                                    <td>{{ $facility->name }}</td>
+                            </tr>
+                            <tr class="table-warning">
+                                <th>住所</th>
+                                    <td >{{ $facility->address }}</td>
+                            </tr>
+                            <tr class="table-warning">
+                                <th>施設内容</th>
+                                    <td>{{ $facility->content }}</td>
+                            </tr>
+                            <tr class="table-warning">
+                                 <th>施設設備</th>
+                                    <td>{{ $facility->equipment }}</td>
+                            </tr>
+                            <tr class="table-warning">
+                                <th>施設料金</th>
+                                    <td>{{ $facility->fee }}</td>
+                            </tr>
+                            <tr class="table-warning">
+                                <th>施設アクセス</th>
+                                    <td>{{ $facility->accsess }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+        </section>
+    </body>
+</html>
--- resources/views/toppage/index.blade.php
+++ resources/views/toppage/index.blade.php
@@ -3,39 +3,12 @@
 @section('content')
     <div class="container">
         <hr color="#000000">
-    <!--    @if (!is_null($headline))-->
-    <!--        <div class="row">-->
-    <!--            <div class="headline col-md-10 mx-auto">-->
-    <!--                <div class="row">-->
-    <!--                    <div class="col-md-6">-->
-    <!--                        <div class="caption mx-auto">-->
-    <!--                            <div class="image">-->
-    <!--                                @if ($headline->image_path)-->
-    <!--                                    <img src="{{ secure_asset('storage/image/' . $headline->image_path) }}">-->
-    <!--                                @endif-->
-    <!--                            </div>-->
-    <!--                            <div class="title p-2">-->
-    <!--                                <h1>{{ Str::limit($headline->name, 70) }}</h1>-->
-    <!--                            </div>-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                    <div class="col-md-6">-->
-    <!--                        <p class="body mx-auto">{{ Str::limit($headline->content, 650) }}</p>-->
-    <!--                    </div>-->
-    <!--                </div>-->
-    <!--            </div>-->
-    <!--        </div>-->
-    <!--    @endif-->
-        <hr color="#000000">
         <div class="row">
             <div class="posts col-md-8 mx-auto mt-3">
                 @foreach($posts as $post)
                     <div class="post">
                         <div class="row">
                             <div class="text col-md-6">
-                                <div class="date">
-                                    {{ $post->updated_at->format('Y年m月d日') }}
-                                </div>
                                 <div class="title">
                                      <a href="{{ route('toppage.facility', ['id' => $post->id]) }}">{{ Str::limit($post->name, 150) }}</a>
                                 </div>
